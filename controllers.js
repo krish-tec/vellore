a89ba9583b2f40f2c631538da1f93c85c806185b
Navaneeth Malingan <navneetnivu07@gmail.com>
2017-06-29 16:02:18 +0530

    Date difference in Dashboard

--- controllers.js
+++ controllers.js
@@ -1,6 +1,7 @@
 app.controller('dashboardController', ['$scope', '$http', '$timeout', '$interval', function($scope, $http, $timeout, $interval) {
 
     $("data_pane").hide();
+    
     $scope.getData = function() {
       $http({
           
@@ -8,7 +9,7 @@ app.controller('dashboardController', ['$scope', '$http', '$timeout', '$interval
           url: 'http://localhost/node/room_status.php'
           
       }).then(function (response) {
-          
+
           // on success
           $scope.spice = response.data.result;
           $("data_pane").show();
@@ -46,38 +47,26 @@ app.controller('reportController', ['$scope', '$http', function($scope, $http) {
 
               response.data.result.forEach(function (dataItem) {
 
-
-
                 var dt = moment(dataItem.date, ["YYYY-MM-DD hh:mm:ssa"]);
                 var now = moment();
                 var duration = moment.duration(now.diff(dt));
                 var dateDiffInSec = duration.asSeconds();
 
-/*
-                console.log(
-                  moment(hours)
-                  .format('D[ day(s)] H[ hour(s)] m[ minute(s)] s[ second(s) ago.]')
-                );
-
-                console.log(dt)
-                console.log(now)
-                console.log(moment(duration).format("HH:mm:ss"))
-                console.log(hours)
-*/
-
                 // Date Format : 2017-06-23 06:49:29pm
                 dataItem.date = dt.format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-                //var dateDiffInSec = Math.abs(new Date() - new Date(dataItem.date));
 
                 if(dateDiffInSec < 60){
                     var conditionTime = dateDiffInSec + " Seconds"
                 } else if(dateDiffInSec > 60 && dateDiffInSec < 3600){
                     var conditionTime = Math.round(dateDiffInSec/60) + " Minutes"
                 }else if(dateDiffInSec > 3600 && dateDiffInSec < 3600*60){
-                    var conditionTime = Math.round(dateDiffInSec/(60*60),1) + " Hours"
+                    var hours = dateDiffInSec/(60*60);
+                    var mins = (dateDiffInSec/(60*60)-Math.floor(hours))*60;
+                    var conditionTime = Math.floor(hours) + " Hours " + Math.floor(mins) + " Minutes";
                 }else if(dateDiffInSec > 3600*24){
-                    var conditionTime = Math.round(dateDiffInSec/(60*60*24),1) + " Days"
+                    var days = dateDiffInSec/(60*60*24);
+                    var hours = (dateDiffInSec/(60*60*24)-Math.floor(days))*24
+                    var conditionTime = Math.floor(days) + " Days " + Math.floor(hours) + " Hours";
                 }
 
                 if(dataItem.status == 1){
